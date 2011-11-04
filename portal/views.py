916ff9873b66323779ed837b954abc046a02ceda
kante <kante@Dr-Gonzos-MacBook-Pro.local>
2011-11-04 15:57:52 -0400

    getting ready to delete all other branches

--- portal/views.py
+++ portal/views.py
@@ -13,45 +13,51 @@ def get_user_redirect(user):
     user_role = user.get_profile().user_role
     
     if user_role == UserRoles.INVESTIGATOR:
-	    return '/investigator/'
+        return '/investigator/'
     elif user_role == UserRoles.PARTICIPANT:
-	    return '/study/'
+        return '/study/'
     else:
-	    return '/unknown_user_role/'
+        return '/unknown_user_role/'
     
 
 def home(request):
-	"""docstring for home"""
-	if request.user.is_authenticated():
-	    return HttpResponseRedirect(get_user_redirect(request.user))
-	else:    
-	    return render_to_response("registration/login.html", locals(), context_instance=RequestContext(request))
+    """docstring for home"""
+    print "tetetetet"
+    
+    if request.user.is_authenticated():
+        return HttpResponseRedirect(get_user_redirect(request.user))
+    else:    
+        return render_to_response("registration/login.html", locals(), context_instance=RequestContext(request))
 
 
 def login(request):
-	"""Try to log in. Redirects to the appropriate error page if something bad
-	happens."""
-	if request.method == 'POST':
-		username = request.POST.get('username', '')
-		password = request.POST.get('password', '')
-		user = auth.authenticate(username=username, password=password)
-		
-		if user is not None and user.is_active:
-			# Correct password, and the user is marked "active"
-			auth.login(request, user)
-			return HttpResponseRedirect(get_user_redirect(user))	
-		else:
-		    # Bad user 
-			return render_to_response("registration/login.html", locals(), context_instance=RequestContext(request))
-	else:
-	    # Strange request. Send them back to the start
-	    return home(request)
+    """Try to log in. Redirects to the appropriate error page if something bad
+    happens."""
+    print "YYHYHYHYHYHY"
+    if request.method == 'POST':
+        username = request.POST.get('username', '')
+        password = request.POST.get('password', '')
+        user = auth.authenticate(username=username, password=password)
+        print "ASDFASDFASDFAS", get_user_redirect(user)
+        
+        if user is not None and user.is_active:
+            # Correct password, and the user is marked "active"
+            auth.login(request, user)
+            return HttpResponseRedirect(get_user_redirect(user))    
+        else:
+            # Bad user 
+            print "BADBAD11111"
+            return render_to_response("registration/login.html", locals(), context_instance=RequestContext(request))
+    else:
+        # Strange request. Send them back to the start
+        print "BADBAD22222"
+        return render_to_response("registration/login.html", locals(), context_instance=RequestContext(request))
 
 
 def logout(request):
-	auth.logout(request)
-	# Redirect to a success page.
-	return HttpResponseRedirect("/")
-	
-	
-	
+    auth.logout(request)
+    # Redirect to a success page.
+    return HttpResponseRedirect("/")
+    
+    
+    

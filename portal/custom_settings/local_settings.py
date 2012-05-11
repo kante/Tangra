c47282dcc010923d5edba88251b0da3724cdcdb7
Arturo MP <amp.interwebs@gmail.com>
2012-05-10 20:57:19 -0400

    compatibility w Django1.4 database and loader settings, general root path

--- portal/custom_settings/local_settings.py
+++ portal/custom_settings/local_settings.py
@@ -1,8 +1,13 @@
 import os
+
 # point this to the Tangra directory on your server
-
-ROOT_PATH = 'c:/Users/kante/Desktop/Tangra/'
-
+# ROOT_PATH = 'c:/Users/kante/Desktop/Tangra/'
+# more general version, but assumes a specific directory structure
+ROOT_PATH = os.path.join(
+                        os.path.dirname(__file__), 
+                        os.path.pardir, 
+                        os.path.pardir
+                        )
 
 # The place to serve static files from
 #STATIC_URL = 'http://192.168.74.136'
@@ -15,13 +20,19 @@ ROOT_PATH = 'c:/Users/kante/Desktop/Tangra/'
 SECRET_KEY = '4=ic*0&yk7rh@zyxbrq*-+&i9tqj16j%m4@0sbwe%plxw(%%3b'
 
 # Database customization
-DATABASE_NAME = os.path.join(ROOT_PATH, 'db/portal.db')             # Or path to database file if using sqlite3.
-DATABASE_ENGINE = 'sqlite3'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_USER = ''             # Not used with sqlite3.
-DATABASE_PASSWORD = ''         # Not used with sqlite3.
-DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
-DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
+# DATABASE_NAME = os.path.join(ROOT_PATH, 'db/portal.db')             # Or path to database file if using sqlite3.
+# DATABASE_ENGINE = 'sqlite3'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+# DATABASE_USER = ''             # Not used with sqlite3.
+# DATABASE_PASSWORD = ''         # Not used with sqlite3.
+# DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
+# DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(ROOT_PATH, 'db/portal.db'),
+    }
+}
 
 # ???
 LOCKDOWN_PASSWORD = ('jetta')

2ccb5b418a3344c366ac65e790a130e940c4e460
kante <kante@omegatutors.com>
2012-07-10 07:07:58 -0700

    added fsess to exe_interface

--- portal/exe_interface/urls.py
+++ portal/exe_interface/urls.py
@@ -5,7 +5,8 @@ from portal.exe_interface.views import *
 urlpatterns = patterns('',
     url(r'^$', testing, name="testing"),
     url(r'^login$', login, name="login"),
-    url(r'^get_current_stage_info$', get_current_stage_info, name="get_current_stage_info")
+    url(r'^get_current_stage_info$', get_current_stage_info, name="get_current_stage_info"),
+    url(r'^fsess$', fsess, name="fsess")
 
 )
 

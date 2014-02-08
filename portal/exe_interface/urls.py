549927cbb588dc96221e04fb46f7f097216d5546
kante <kante@omegatutors.com>
2014-02-08 13:54:59 -0500

    appropriate results are now being returned, sorted by relevance based on participant's answers. need to format the results page and implement all the other sections of the survey

--- portal/exe_interface/urls.py
+++ portal/exe_interface/urls.py
@@ -6,7 +6,7 @@ urlpatterns = patterns('',
     url(r'^$', testing, name="testing"),
     url(r'^login$', login, name="login"),
     url(r'^get_current_stage_info$', get_current_stage_info, name="get_current_stage_info"),
-    url(r'^fsess$', fsess, name="fsess")
-
+    url(r'^fsess$', fsess, name="fsess"),
+    url(r'^get_user_data$', get_user_data, name="get_user_data")
 )
 

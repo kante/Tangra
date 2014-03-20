399d2aa570bd4f7bfe1e08f52ca752edee84e5be
kante <kante@omegatutors.com>
2014-03-20 16:03:40 -0400

    Finished testing the most basic save/get data methods. Time to do fancier stuff with multiple stages

--- public_api/unit_tests/test_keyless_data.py
+++ public_api/unit_tests/test_keyless_data.py
@@ -38,6 +38,17 @@ class SingleStageTestCase(TangraTestCase):
         
         expected_data = [data_to_save["data"]]
         self.perform_and_verify_query('/public_api/get_data', expected_data)
+    
+    
+    def test_saving_multiple_strings(self):
+        data_to_save = {"data" : "This is a string I am saving"}
+        self.perform_and_verify_query('/public_api/save_data', SuccessResponse.success_string, "POST", data_to_save)
+        
+        more_data = {"data" : "ERUJSADFNN848349534%#$%3453450912=0--09234][]"}
+        self.perform_and_verify_query('/public_api/save_data', SuccessResponse.success_string, "POST", more_data)
+        
+        expected_data = [data_to_save["data"], more_data["data"]]
+        self.perform_and_verify_query('/public_api/get_data', expected_data)
 
 
 
